02673903536f9f29385463ae24aea7b22f1c0f85
Oleksandr Havryliuk <sashagavgavgav@gmail.com>
2019-04-11 01:47:49 +0300

    add auth module, and firebase settings

---
+++ app/src/editor/java/com/oleksandr/havryliuk/editor/auth/forgotpassword/ForgotPasswordPresenter.java
@@ -0,0 +1,39 @@
+package com.oleksandr.havryliuk.editor.auth.forgotpassword;
+
+import android.text.TextUtils;
+
+import com.oleksandr.havryliuk.editor.auth.AuthenticationActivity;
+
+public class ForgotPasswordPresenter implements ForgotPasswordContract.IForgotPasswordPresenter {
+
+    private ForgotPasswordFragment fragment;
+    private ForgotPasswordContract.IForgotPasswordView view;
+
+    public ForgotPasswordPresenter(ForgotPasswordFragment fragment, ForgotPasswordContract.IForgotPasswordView view) {
+        this.fragment = fragment;
+        this.view = view;
+    }
+
+    @Override
+    public void sendRecoverCodeClick() {
+        validateInput();
+    }
+
+    @Override
+    public void showSignIn() {
+        ((AuthenticationActivity) fragment.getActivity()).showSignIn();
+    }
+
+    private void validateInput() {
+        view.hideEmailError();
+
+        String email = view.getEmail();
+
+        if (TextUtils.isEmpty(email)) {
+            view.showEmailError();
+            return;
+        }
+
+        ((AuthenticationActivity) fragment.getActivity()).sendRecoverCode(view.getEmail());
+    }
+}
---
+++ app/src/editor/java/com/oleksandr/havryliuk/editor/auth/signin/SignInView.java
@@ -0,0 +1,97 @@
+package com.oleksandr.havryliuk.editor.auth.signin;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.oleksandr.havryliuk.tvcontentcontroller.R;
+
+public class SignInView implements SignInContract.ISignInView {
+    private Button signInBtn;
+    private TextView signUpTv, forgotPasswordTv;
+    private EditText loginEt, passwordEt;
+    private ImageView googleImg;
+    //private LoginButton facebookBtn;
+
+    private SignInContract.ISignInPresenter presenter;
+
+    public SignInView() {
+
+    }
+
+    public void setPresenter(SignInContract.ISignInPresenter presenter) {
+        this.presenter = presenter;
+    }
+
+    public void init(View root) {
+        googleImg = root.findViewById(R.id.google_signIn_button);
+        signInBtn = root.findViewById(R.id.signin_btn);
+        signUpTv = root.findViewById(R.id.signup_txt);
+        forgotPasswordTv = root.findViewById(R.id.forgot_password_txt);
+        loginEt = root.findViewById(R.id.login_et);
+        passwordEt = root.findViewById(R.id.password_et);
+        initListeners();
+    }
+
+    private void initListeners() {
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                presenter.signInClick();
+            }
+        });
+
+        googleImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                presenter.googleSignInClick();
+            }
+        });
+
+        signUpTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                presenter.showSignUp();
+            }
+        });
+
+        forgotPasswordTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                presenter.showForgotPassword();
+            }
+        });
+    }
+
+    @Override
+    public void hideLoginError() {
+        loginEt.setError(null);
+    }
+
+    @Override
+    public void hidePasswordError() {
+        passwordEt.setError(null);
+    }
+
+    @Override
+    public void showLoginError() {
+        loginEt.setError("Please enter login!");
+    }
+
+    @Override
+    public void showPasswordError() {
+        passwordEt.setError("Please enter password!");
+    }
+
+    @Override
+    public String getLoginText() {
+        return loginEt.getText().toString();
+    }
+
+    @Override
+    public String getPasswordText() {
+        return passwordEt.getText().toString();
+    }
+}
