3ecb471c078bb7e143c498efe4c966e04265eaba
Oleksandr Havryliuk <sashagavgavgav@gmail.com>
2019-05-28 12:34:04 +0300

    moving res files to right packages

--- app/src/client/java/com/oleksandr/havryliuk/tvcontentcontroller/client/MainActivity.java
+++ app/src/client/java/com/oleksandr/havryliuk/tvcontentcontroller/client/MainActivity.java
@@ -1,4 +1,6 @@
-package com.oleksandr.havryliuk.tvcontentcontroller;
+package com.oleksandr.havryliuk.tvcontentcontroller.client;
+
+import com.oleksandr.havryliuk.tvcontentcontroller.R;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
